719d2aab6d35df0f9ebffda8a452289edf808538
spflow <sean.pascoe@gmail.com>
2016-07-20 16:36:27 -0600

    initial commit, simple app complete

---
+++ js/main.js
@@ -0,0 +1,72 @@
+function userSelect(choice) {
+  var bannerEl = document.getElementById("result-banner");
+  bannerEl.classList = "col s12 white-text";
+  var userChoice = choice;
+  printChoice('user-choice', userChoice, 'User');
+  compSelect(userChoice);
+}
+
+function compSelect(userChoice) {
+  var choices = ["Rock", "Paper", "Scissors"];
+  var randNum = Math.floor(Math.random()*3);
+  var compChoice = choices[randNum];
+  printChoice('comp-choice', compChoice, 'Life');
+  roshambo(userChoice, compChoice);
+}
+
+function roshambo(user, comp) { roshambo: {
+  if (user === comp) {
+    tie();
+    break roshambo;
+  } else {
+    switch (user) {
+      case "Rock":
+        if(comp === "Paper") {
+          youLose();
+        } else {
+          youWin();
+        }
+        break;
+      case "Paper":
+        if(comp === "Scissors") {
+          youLose();
+        } else {
+          youWin();
+        }
+        break;
+      case "Scissors":
+        if(comp === "Rock") {
+          youLose();
+        } else {
+          youWin();
+        }
+        break;
+    }
+  }
+}}
+
+function youLose() {
+  var el = document.getElementById("result");
+  var bannerEl = document.getElementById("result-banner");
+  el.innerHTML = '<i class="material-icons result-icons">thumb_down</i> You Lose!!';
+  bannerEl.classList += " red darken-1"
+}
+
+function youWin() {
+  var el = document.getElementById("result");
+  var bannerEl = document.getElementById("result-banner");
+  el.innerHTML = '<i class="material-icons result-icons">thumb_up</i> You Win!!';
+  bannerEl.classList += " green darken-1"
+}
+
+function tie() {
+  var el = document.getElementById("result");
+  var bannerEl = document.getElementById("result-banner");
+  el.innerHTML = '<i class="material-icons result-icons">thumbs_up_down</i> It\'s a Tie';
+  bannerEl.classList += " yellow darken-3"
+}
+
+function printChoice(playerID, choice, player) {
+  var el = document.getElementById(playerID);
+  el.innerHTML = player + ": " + choice;
+}
